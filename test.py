b0977ba75c6b4804173367d69bfa414331d480d2
azz212 <21202893@qq.com>
2024-06-09 12:41:02 +0800

    app test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from quart import Quart, request
 app = Quart(__name__)
-@app.route('/chat', methods=['POST'])
+@app.route('/chat', methods=['POST',"GET"])
 async def chat():
    data = await request.get_json()
    return {'response': 'Hello, World'}
