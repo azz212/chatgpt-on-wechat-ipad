1f22d9d646d8cbefd0b6243d1c2aa6d78c15592f
azz212 <21202893@qq.com>
2024-06-28 23:25:52 +0800

    idiom

--- run_setting.py
+++ run_setting.py
@@ -1,12 +1,18 @@
-
+import urllib.parse
 from channel.wechat.iPadWx import iPadWx
 from config import conf,load_config
 
 load_config()
 wx = iPadWx()
 port = conf().get('wechatipad_port')
-#port = '5711'
-print(port)
 http_hook = conf().get('http_hook')
-print(http_hook)
-wx.set_callback_url(http_hook)
+parsed_url = urllib.parse.urlparse(http_hook)
+protocol = parsed_url.scheme
+domain = parsed_url.netloc.split(':')[0]
+port = parsed_url.netloc.split(':')[1]
+path = parsed_url.path
+port = '5711'
+# 组合
+combined_url = urllib.parse.urlunparse((protocol, domain + ':' + port, path, '', '', ''))
+
+wx.set_callback_url(combined_url)
